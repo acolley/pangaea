12bbe5bcdc36a975e9969aaa406868327e10aa59
acolley <alnessy@hotmail.com>
2014-07-24 16:36:47 +0100

    Update sequence.lua

--- ai/sequence.lua
+++ ai/sequence.lua
@@ -1,19 +1,21 @@
 local function new(...)
-   local sequence = {}
-   sequence.index = 1
-   sequence.behaviours = {...}
-   sequence.process = function(self, cxt)
-       --if arg[#arg] == "-debug" then require("mobdebug").start() end
-       local status = "success"
-       while status == "success" and self.index <= #self.behaviours do
-           status = self.behaviours[self.index]:process(cxt)
-           self.index = self.index + 1
-       end
-       if status == "failure" then
-           self.index = 1
-       end
-       return status
-   end
-   return sequence
+    local sequence = {}
+    sequence._index = 1
+    sequence.behaviours = {...}
+    sequence.process = function(self, cxt)
+        --if arg[#arg] == "-debug" then require("mobdebug").start() end
+        local status = "success"
+        while status == "success" and self._index <= #self.behaviours do
+            status = self.behaviours[self._index]:process(cxt)
+            if status == "success" then
+                self._index = self._index + 1
+            end
+        end
+        if status == "failure" or self._index > #self.behaviours then
+            self._index = 1
+        end
+        return status
+    end
+    return sequence
 end
 return new
