8d152d053cc280c30e3ee75914cc9c5590732be3
acolley <alnessy@hotmail.com>
2014-07-28 23:23:15 +0100

    bug fix in succeeder behaviour

--- ai/model/succeeder.lua
+++ ai/model/succeeder.lua
@@ -3,7 +3,7 @@ local SucceederBehaviour = require "ai/execution/succeeder"
 local function new(behaviour)
     local succeeder = {}
     succeeder.behavour = behaviour
-    succeeder.create_executor(self, executorbt)
+    succeeder.create_executor = function(self, executorbt)
         return SucceederBehaviour(executorbt, self)
     end
     return succeeder
